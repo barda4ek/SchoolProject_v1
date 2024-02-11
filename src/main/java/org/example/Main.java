efb2c3eb5fb22e09beafcef2629fa0d28bd8aa27
zerosixthree <mikhailsychov111@gmail.com>
2024-02-12 00:29:52 +0300

    Z

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-       /* check merge 3 branch
+       /* check merge Z branch
         test for main*/
         Car car = new Car("aaa", 111);
 
