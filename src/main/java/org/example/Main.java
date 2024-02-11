5bf07f38cdb7e5e236ba28edd7c78d9deec3c16b
barda4ek <evgeniyprrp@gmail.com>
2024-02-11 11:43:30 +0300

    commit5 11.02

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,25 +7,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        /*Eight program = new Eight();
-        program.getNumber();
-        program.cunter(k);
-        System.out.println(program.array);*/
-
-        //StreamLearn streamClass = new StreamLearn();
-        //System.out.println(streamClass.objectPredicate.test(new Car("ba")));
-
-
-       /* object1.setName("Антон", 555);
-        object2.setName("Женя", 777);*/
-
-        //System.out.println(object1.getName() + "\n" + object2.getName() + "\n" + NewClass.newCost);
-
-        /*Car car = new Car("toyota", 50000);
-        SportCar sportCar1 = new SportCar("Lexus", 100000);
-
-        sportCar1.setNewCost(40000);
-        System.out.println(sportCar1.getCost());*/
 
         Car car = new Car("aaa", 111);
         Scanner scan = new Scanner(System.in);
