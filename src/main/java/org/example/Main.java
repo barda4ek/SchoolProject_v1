915804dade0516b797f50857d7128fa95debfd74
zerosixthree <mikhailsychov111@gmail.com>
2024-02-11 23:01:18 +0300

    Merge comment

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-       /* Michael's and Evgenii branch
+       /* 2233323232323 branch
         test for main*/
         Car car = new Car("aaa", 111);
 
